ee86d4a5993813cf5e061a2ed06ab32939d4feb4
Patrik Hartwig <patrik.hartwig@megaport.com>
2016-11-22 16:47:47 +1000

    working prototype

--- signaling-server.js
+++ signaling-server.js
@@ -10,19 +10,19 @@ var PORT = 8080;
 var express = require('express');
 var http = require('http');
 var main = express();
-var server = http.createServer(main)
+var server = http.createServer(main);
 var io = require('socket.io').listen(server);
 io.set('log level', 1);
 
 server.listen(PORT, null, function() {
-  console.log("Listening on port " + PORT);
+  console.log('Listening on port ' + PORT);
 });
 
 main.get('/', function(req, res) {
-  res.sendfile('./client.html');
+  res.sendfile('./index.html');
 });
 main.get('/index.html', function(req, res) {
-  res.sendfile('./client.html');
+  res.sendfile('./index.html');
 });
 main.get('/client.html', function(req, res) {
   res.sendfile('./client.html');
@@ -32,12 +32,12 @@ main.get('/client.html', function(req, res) {
 
 io.sockets.on('connection', function(socket) {
 
-  console.log("[" + socket.id + "] connection accepted");
+  console.log('[' + socket.id + '] connection accepted');
   socket.on('disconnect', function() {
     io.sockets.emit('removePeer', socket.id);
   });
 
-  Object.keys(io.sockets.connected).forEach((sockId) => {
+  Object.keys(io.sockets.connected).forEach(sockId => {
     var iSock = io.sockets.connected[sockId];
     if (!iSock.connected) return;
     if (iSock.id == socket.id) return;
@@ -54,7 +54,7 @@ io.sockets.on('connection', function(socket) {
 
   socket.on('relayICECandidate', function(iceObj) {
     var peer_id = iceObj.peer_id;
-    console.log("[" + socket.id + "] relaying ICE candidate to [" + peer_id + "] ", iceObj.ice_candidate);
+    console.log('[' + socket.id + '] relaying ICE candidate to [' + peer_id + '] ', iceObj.ice_candidate);
 
     if (peer_id in io.sockets.connected) {
       io.sockets.connected[peer_id].emit('iceCandidate', {
@@ -66,7 +66,7 @@ io.sockets.on('connection', function(socket) {
 
   socket.on('relaySessionDescription', function(config) {
     var peer_id = config.peer_id;
-    console.log("[" + socket.id + "] relaying session description to [" + peer_id + "] ", config.session_description);
+    console.log('[' + socket.id + '] relaying session description to [' + peer_id + '] ', config.session_description);
 
     if (peer_id in io.sockets.connected) {
       io.sockets.connected[peer_id].emit('sessionDescription', {
