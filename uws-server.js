f22d6bb53ce870bc8aa863b01902e0f80fcd547b
Patrik Hartwig <patrik.hartwig@megaport.com>
2017-03-06 17:21:31 +1000

    still experimenting

--- uws-server.js
+++ uws-server.js
@@ -21,12 +21,92 @@ var socketServer = function(port, cb) {
 
   var clients = [];
 
-  wss.on('connection', function(ws) {
+  var clientOp = function(ws) {
+    Object.assign(this, {
+      id: generateUID(),
+      socket: ws,
+      connected: true
+    });
+
+    var eventsRegister = [];
+    this.on = function(label, cb) {
+      if (typeof cb != 'function') return;
+      var namespace = label.split('.');
+      label = namespace.pop();
+      namespace = namespace.join('.');
+      eventsRegister.push({
+        label: label,
+        namespace: namespace,
+        func: cb
+      });
+    };
+
+    function trigger(label, data) {
+      eventsRegister
+        .filter(e => (e.label == label))
+        .forEach(e => {
+          e.func(...data);
+        });
+    }
+
+    this.send = function(label, data) {
+      if (this.connected)
+        ws.send(JSON.stringify([label, data]));
+    };
+
+
     ws.on('message', console.log);
-    ws.on('close', console.log);
-    ws.send('something :|');
+    ws.on('close', () => {
+      this.connected = false;
+    });
+
+  };
+
+  wss.on('connection', function(ws) {
+    var clientConn = new clientOp(ws);
+    clients.push(clientConn);
+    trigger('connect', [clientConn]);
   });
-}
+
+  var eventsRegister = [];
+  this.on = function(label, cb) {
+    if (typeof cb != 'function') return;
+    var namespace = label.split('.');
+    label = namespace.pop();
+    namespace = namespace.join('.');
+    eventsRegister.push({
+      label: label,
+      namespace: namespace,
+      func: cb
+    });
+  };
+
+  function trigger(label, data) {
+    eventsRegister
+      .filter(e => (e.label == label))
+      .forEach(e => {
+        e.func(...data);
+      });
+  }
+
+  function generateUID() {
+    var firstPart = (Math.random() * 46656) | 0;
+    var secondPart = (Math.random() * 46656) | 0;
+    firstPart = ('000' + firstPart.toString(36)).slice(-3);
+    secondPart = ('000' + secondPart.toString(36)).slice(-3);
+    return firstPart + secondPart;
+  }
+
+};
+
+
 
 
 var pws = new socketServer(8585);
+
+pws.on('connect', (client => {
+  console.log(client);
+  client.send('hullo', {
+    w: 123
+  });
+}));
